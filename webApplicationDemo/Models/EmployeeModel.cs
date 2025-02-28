3e5ce4b22be0dedd63b639ed23a4e09ac0188775
WeiC02 <weichuen6738@gmail.com>
2025-02-28 11:28:26 +0800

    Add File

---
+++ webApplicationDemo/Models/EmployeeModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace webApplicationDemo.Models
+{
+    public class EmployeeModel
+    {
+
+            public int EmployeeID { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            public string FullName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [StringLength(150)]
+            public string Email { get; set; }
+
+            [StringLength(20)]
+            public string Phone { get; set; }
+
+            [DataType(DataType.Date)]
+            public DateTime HireDate { get; set; }
+
+            public byte[] EmployeeImage { get; set; }
+
+    }
+}
