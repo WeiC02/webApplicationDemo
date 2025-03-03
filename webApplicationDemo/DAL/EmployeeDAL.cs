ff0bba1b4416a272688fde193e22786f40b28e4b
wenbin <wenbinchong1124@gmail.com>
2025-03-03 15:40:24 +0800

    asdasd
    asdasd

--- webApplicationDemo/DAL/EmployeeDAL.cs
+++ webApplicationDemo/DAL/EmployeeDAL.cs
@@ -124,6 +124,76 @@ namespace webApplicationDemo.DAL
             }
         }
 
+        public EmployeeModel? GetEmployeeById(int employeeId)
+        {
+            EmployeeModel? employee = null;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    string query = "SELECT EmployeeID, FullName, Email, Phone, HireDate, EmployeeImage, CompanyID FROM Employee WHERE EmployeeID = @EmployeeID";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@EmployeeID", employeeId);
+                        connection.Open();
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                employee = new EmployeeModel
+                                {
+                                    EmployeeID = reader.GetInt32(0),
+                                    FullName = reader.GetString(1),
+                                    Email = reader.GetString(2),
+                                    Phone = reader.GetString(3),
+                                    HireDate = reader.GetDateTime(4),
+                                    EmployeeImage = reader.IsDBNull(5) ? null : (byte[])reader["EmployeeImage"],
+                                    CompanyID = reader.GetInt32(6)
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("[GetEmployeeById] - " + ex.Message);
+            }
+
+            return employee;
+        }
+
+        public bool UpdateEmployee(EmployeeModel employee)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    string query = "UPDATE Employee SET FullName = @FullName, Email = @Email, Phone = @Phone, HireDate = @HireDate, EmployeeImage = @EmployeeImage WHERE EmployeeID = @EmployeeID";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@FullName", employee.FullName);
+                        command.Parameters.AddWithValue("@Email", employee.Email);
+                        command.Parameters.AddWithValue("@Phone", employee.Phone);
+                        command.Parameters.AddWithValue("@HireDate", employee.HireDate);
+                        command.Parameters.Add("@EmployeeImage", SqlDbType.VarBinary).Value = (object?)employee.EmployeeImage ?? DBNull.Value;
+                        command.Parameters.AddWithValue("@EmployeeID", employee.EmployeeID);
+
+                        return command.ExecuteNonQuery() > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("[UpdateEmployee] - " + ex.Message);
+            }
+        }
+
 
 
     }
